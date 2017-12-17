e23f437b1e225e2a623adff270905537d780b187
KJ <xiankai@gmail.com>
2017-12-17 09:30:25 +0800

    Add sentosa and yew tee park

--- compiled.js
+++ compiled.js
@@ -68,7 +68,7 @@ function addToMap(layer) {
   return markers;
 }
 
-fetchLocal("https://cdn.rawgit.com/xiankai/fc4260e305d1339756a3e1a02b495939/raw/1509d16007b0a74f3cec63f5eb5715da5662300c/all.geojson").then(function (data) {
+fetchLocal("https://cdn.rawgit.com/xiankai/fc4260e305d1339756a3e1a02b495939/raw/5c577ab887186ee300896c20d314d0a11547e901/all.geojson").then(function (data) {
   var _ref, _ref2;
 
   gyms = data;
--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function addToMap(layer) {
 }
 
 fetchLocal(
-  "https://cdn.rawgit.com/xiankai/fc4260e305d1339756a3e1a02b495939/raw/1509d16007b0a74f3cec63f5eb5715da5662300c/all.geojson"
+  "https://cdn.rawgit.com/xiankai/fc4260e305d1339756a3e1a02b495939/raw/5c577ab887186ee300896c20d314d0a11547e901/all.geojson"
 ).then(data => {
   gyms = data;
 
