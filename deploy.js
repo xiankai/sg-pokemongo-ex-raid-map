7c95741e497f1d57689b47f4487e7b5313feb92f
KJ <xiankai@gmail.com>
2018-02-25 20:27:04 +0800

    Parameterized deployments. Closes #8

---
+++ deploy.js
@@ -0,0 +1,31 @@
+const { execSync } = require('child_process');
+const ghpages = require('gh-pages');
+const id = process.argv[2] || 'singapore';
+execSync('rm -f .env.production .env.local.production');
+execSync(`ln -s ${id}.env .env.production`);
+execSync(
+	`echo "REACT_APP_VERSION=${new Date().getDate()} ${new Date()
+		.toString()
+		.slice(4, 7)}" > .env.local.production`
+);
+execSync(`yarn build`);
+
+ghpages.publish(
+	'build',
+	{
+		branch: id === 'singapore' ? 'gh-pages' : 'master',
+		repo:
+			id === 'singapore'
+				? 'https://github.com/xiankai/sg-pokemongo-ex-raid-map'
+				: `https://github.com/pokemongo-exraid-maps/${id}`,
+		// message: ''
+	},
+	err => {
+		if (err) {
+			console.error(err);
+		} else {
+			execSync('rm .env.production .env.local.production');
+			console.log(`Successfully deployed for ${id}`);
+		}
+	}
+);
